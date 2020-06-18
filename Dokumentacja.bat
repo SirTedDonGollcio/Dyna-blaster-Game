efcc15838836d239bb184fb685d46fe86c251967
unknown <dfrajda>
2020-06-19 00:13:16 +0200

    Dodano jar i skrypt uruchamiający tworzenie dokumentacji

---
+++ Dokumentacja.bat
@@ -0,0 +1,14 @@
+cd BombowaFrajda2\src\Client
+javadoc -d dokumentacja *.java -private
+cd ..
+cd Game
+javadoc -d dokumentacja *.java -private
+cd ..
+cd GUI
+javadoc -d dokumentacja *.java -private
+cd ..
+cd Parameters
+javadoc -d dokumentacja *.java -private
+cd ..
+cd Animations
+javadoc -d dokumentacja *.java -private
